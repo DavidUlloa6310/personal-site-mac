8f60dabdba66a0a3c252344cb8ec316548259e81
David Ulloa <dulloa6310@gmail.com>
2022-09-16 19:09:20 -0400

    Added pointer cursor to SkipButton

--- components/SkipButton.jsx
+++ components/SkipButton.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function SkipButton(props) {
   return (
     <div
-      className="absolute right-10 bottom-10 flex h-10 w-24 items-center justify-center border-2 border-black bg-white p-1 shadow-mac"
+      className="absolute right-10 bottom-10 flex h-10 w-24 cursor-macPointer items-center justify-center border-2 border-black bg-white p-1 shadow-mac"
       onClick={props.onClick}
     >
       <h1 className=" font-chicago">Skip</h1>
